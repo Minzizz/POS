7d86045bd2ad39a71514a4ed858b7a92a09cc061
Amin Aziz Sudjud <azizsudjud@gmail.com>
2026-02-24 12:46:52 +0700

    Tambahan

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::prefix('product')->group(function () {
-    Route::get('/category/food-beverage', [CategoryController::class, 'foodBeverage'])->name('category.foodBeverage');
+    Route::get('/category/food-beverage', [CategoryController::class, 'foodBeverage'])
+    ->name('category.foodBeverage');
 
-    Route::get('/category/beauty-health', [CategoryController::class, 'beautyHealth'])->name('category.beautyHealth');
+    Route::get('/category/beauty-health', [CategoryController::class, 'beautyHealth'])
+    ->name('category.beautyHealth');
 
-    Route::get('/category/home-care', [CategoryController::class, 'homeCare'])->name('category.homeCare');
+    Route::get('/category/home-care', [CategoryController::class, 'homeCare'])
+    ->name('category.homeCare');
 
-    Route::get('/category/baby-kid', [CategoryController::class, 'babyKid'])->name('category.babyKid');
+    Route::get('/category/baby-kid', [CategoryController::class, 'babyKid'])
+    ->name('category.babyKid');
 });
 
 Route::get('/user/{userID}/name/{name}', function ($userID, $name) {
